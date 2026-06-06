4593f38c75ccc7aaf19dd1b613f84590523bd0a2
Jad Abdallah <jadabdallah260@gmail.com>
2026-06-06 14:01:00 +0300

    scrollable button (Css+js)

--- car_dealer_website/script.js
+++ car_dealer_website/script.js
@@ -7,3 +7,30 @@ function showMenu() {
 function hideMenu() {
   navLinks.style.right = "-220px";
 }
+class ScrollToTop {
+  constructor(buttonId) {
+    this.button = document.getElementById(buttonId);
+
+    if (!this.button) return;
+
+    window.addEventListener("scroll", () => this.toggleButton());
+    this.button.addEventListener("click", () => this.scrollToTop());
+  }
+
+  toggleButton() {
+    if (window.scrollY > 300) {
+      this.button.classList.add("show");
+    } else {
+      this.button.classList.remove("show");
+    }
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+}
+
+new ScrollToTop("scrollTopBtn");
